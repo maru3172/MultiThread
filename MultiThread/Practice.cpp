3e64085b353662e040b278baa7a0b64130146818
maru3172 <96215725+maru3172@users.noreply.github.com>
2025-09-11 15:06:58 +0900

    Atomic
    VOLATILE은 atomic하지 않지만 atomic과 기능이 비슷하다 atomic도 순서를 지키는 것을 원칙으로 한다, 반드시 순서대로 실행한다

--- MultiThread/Practice.cpp
+++ MultiThread/Practice.cpp
@@ -3,8 +3,9 @@
 #include <mutex>
 #include <chrono>
 #include <vector>
+#include <atomic>
 
-volatile int sum = 0;
+std::atomic<int> sum = 0;
 std::mutex mtx;
 using namespace std::chrono;
 
@@ -14,8 +15,8 @@ int main()
 {
 	for (int num_threads = 1; num_threads <= 16; num_threads *= 2) {
 		sum = 0;
-		auto start = high_resolution_clock::now();
 		std::vector<std::thread> threads;
+		auto start = high_resolution_clock::now();
 		for (int i = 0; i < num_threads; ++i)
 			threads.emplace_back(worker, 50000000 / num_threads);
 		for (auto& t : threads)
@@ -40,8 +41,6 @@ int main()
 
 void worker(const int& loopCount)
 {
-	mtx.lock();
 	for (int i = 0; i < loopCount; ++i)
 		sum = sum + 2;
-	mtx.unlock();
 }
