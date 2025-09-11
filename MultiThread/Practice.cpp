9839ef43f6bfbcaf48fa193e2aa201715b2b290d
maru3172 <96215725+maru3172@users.noreply.github.com>
2025-09-11 14:48:10 +0900

    시간 측정

--- MultiThread/Practice.cpp
+++ MultiThread/Practice.cpp
@@ -1,26 +1,46 @@
 #include <iostream>
 #include <thread>
 #include <mutex>
+#include <chrono>
+#include <vector>
 
 int sum;
 std::mutex mtx;
+using namespace std::chrono;
 
-void worker();
+void worker(const int& loopCount);
 
 int main()
 {
-	std::thread t1{ worker };
-	std::thread t2{ worker };
-	t1.join();
-	t2.join();
+	for (int num_threads = 1; num_threads <= 16; num_threads *= 2) {
+		auto start = high_resolution_clock::now();
+		std::vector<std::thread> threads;
+		for (int i = 0; i < num_threads; ++i)
+			threads.emplace_back(worker, 10000000 / num_threads);
+		for (auto& t : threads)
+			t.join();
+		auto end = high_resolution_clock::now();
+		auto duration = duration_cast<milliseconds>(end - start);
+		std::cout << num_threads << "multi: " << duration.count() << "ms" << std::endl;
+	}
+	std::cout << "sum = " << sum << std::endl;
+
+	sum = 0;
+	auto start = high_resolution_clock::now();
+	for (int i = 0; i < 50000000; ++i)
+		sum = sum + 2;
+	auto end = high_resolution_clock::now();
+	auto duration = duration_cast<milliseconds>(end - start);
+	std::cout << "single:" << duration.count() << "ms" << std::endl;
 
 	std::cout << "sum = " << sum << std::endl;
+
 }
 
-void worker()
+void worker(const int& loopCount)
 {
 	mtx.lock();
-	for (int i = 0; i < 25000000; ++i)
+	for (int i = 0; i < loopCount; ++i)
 		sum = sum + 2;
 	mtx.unlock();
 }
