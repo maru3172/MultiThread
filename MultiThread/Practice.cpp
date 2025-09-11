4ccd5d607edcdbbc58d5a27a09332a634ec62fc8
maru3172 <96215725+maru3172@users.noreply.github.com>
2025-09-11 14:51:13 +0900

    성능 측정2

--- MultiThread/Practice.cpp
+++ MultiThread/Practice.cpp
@@ -13,17 +13,18 @@ void worker(const int& loopCount);
 int main()
 {
 	for (int num_threads = 1; num_threads <= 16; num_threads *= 2) {
+		sum = 0;
 		auto start = high_resolution_clock::now();
 		std::vector<std::thread> threads;
 		for (int i = 0; i < num_threads; ++i)
-			threads.emplace_back(worker, 10000000 / num_threads);
+			threads.emplace_back(worker, 50000000 / num_threads);
 		for (auto& t : threads)
 			t.join();
 		auto end = high_resolution_clock::now();
 		auto duration = duration_cast<milliseconds>(end - start);
 		std::cout << num_threads << "multi: " << duration.count() << "ms" << std::endl;
+		std::cout << "sum = " << sum << std::endl;
 	}
-	std::cout << "sum = " << sum << std::endl;
 
 	sum = 0;
 	auto start = high_resolution_clock::now();
