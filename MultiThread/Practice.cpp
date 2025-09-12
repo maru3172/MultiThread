36078130ad3e59dc0e21a6d150822e91ece1ae35
maru3172 <maru3172@naver.com>
2025-09-12 14:05:17 +0900

    alignas 사용, 최선의 답

--- MultiThread/Practice.cpp
+++ MultiThread/Practice.cpp
@@ -5,9 +5,14 @@
 #include <vector>
 #include <atomic>
 
+struct NUM {
+	alignas(64) volatile int value;
+};
+
 const int MAX_THREADS = 16;
+const int CACHE_LINE_SIZE_INT = 16;
 volatile int sum = 0;
-volatile int array_sum[MAX_THREADS] = { 0 };
+NUM array_sum[MAX_THREADS] = { 0 };
 std::mutex mtx;
 using namespace std::chrono;
 
@@ -23,8 +28,8 @@ int main()
 			threads.emplace_back(worker, i, 50000000 / num_threads);
 		for (int i = 0; i < num_threads; ++i) {
 			threads[i].join();
-			sum = sum + array_sum[i];
-			array_sum[i] = 0;
+			sum = sum + array_sum[i].value;
+			array_sum[i].value = 0;
 		}
 		auto end = high_resolution_clock::now();
 		auto duration = duration_cast<milliseconds>(end - start);
@@ -48,5 +53,5 @@ int main()
 void worker(const int& thread_id, const int& loopCount)
 {
 	for (int i = 0; i < loopCount; ++i)
-		array_sum[thread_id] = array_sum[thread_id] + 2;
+		array_sum[thread_id].value = array_sum[thread_id].value + 2;
 }
