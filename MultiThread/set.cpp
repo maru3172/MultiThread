2a529e322d940146eea0632d5c9e75ca67c486e8
maru3172 <96215725+maru3172@users.noreply.github.com>
2025-10-02 15:21:43 +0900

    낙천적 동기화

--- MultiThread/set.cpp
+++ MultiThread/set.cpp
@@ -130,6 +130,8 @@ private:
 	std::mutex mtx;
 };
 
+
+
 class F_SET {
 public:
 	F_SET() {
@@ -238,10 +240,160 @@ public:
 	}
 
 private:
+	bool validate(NODE* prev, NODE* curr)
+	{
+		NODE* node = prev;
+		while (node) {
+			if (node == prev)
+				return prev->next == curr;
+			node = node->next;
+		}
+		return false;
+	}
+
 	NODE* head, * tail;
 };
 
-C_SET set;
+class O_SET {
+public:
+	O_SET() {
+		head = new NODE(std::numeric_limits<int>::min());
+		tail = new NODE(std::numeric_limits<int>::max());
+		head->next = tail;
+	}
+
+	~O_SET()
+	{
+		clear();
+		delete head;
+		delete tail;
+	}
+
+	void clear()
+	{
+		NODE* curr = head->next;
+		while (curr != tail) {
+			NODE* temp = curr;
+			curr = curr->next;
+			delete temp;
+		}
+		head->next = tail;
+	}
+
+	bool add(int x)
+	{
+		while (true)
+		{
+			auto prev = head;
+			auto curr = prev->next;
+
+			while (curr->value < x) {
+				prev = curr;
+				curr = curr->next;
+			}
+
+			prev->lock(); curr->lock();
+			if (false == validate(x, prev, curr)) {
+				prev->unlock(); curr->unlock();
+				continue;
+			}
+			if (curr->value == x) {
+				prev->unlock(); curr->unlock();
+				return false;
+			}
+
+			auto newNode = new NODE(x);
+			newNode->next = curr;
+			prev->next = newNode;
+			prev->unlock(); curr->unlock();
+			return true;
+		}
+	}
+
+	bool remove(int x)
+	{
+		while (true)
+		{
+
+			auto prev = head;
+			auto curr = prev->next;
+
+			while (curr->value < x) {
+				prev = curr;
+				curr = curr->next;
+			}
+
+			prev->lock(); curr->lock();
+			if (false == validate(x, prev, curr)) {
+				prev->unlock(); curr->unlock();
+				continue;
+			}
+
+			if (curr->value == x) {
+				prev->next = curr->next;
+				prev->unlock(); curr->unlock();
+				// delete curr;
+				return true;
+			}
+
+			prev->unlock(); curr->unlock();
+			return false;
+		}
+	}
+
+	bool contains(int x)
+	{
+		while (true)
+		{
+			auto prev = head;
+			auto curr = prev->next;
+
+			while (curr->value < x) {
+				prev = curr;
+				curr = curr->next;
+			}
+			prev->lock(); curr->lock();
+			if (false == validate(x, prev, curr)) {
+				prev->unlock(); curr->unlock();
+				continue;
+			}
+
+			if (curr->value == x) {
+				prev->unlock(); curr->unlock();
+				return true;
+			}
+
+			prev->unlock(); curr->unlock();
+			return false;
+		}
+	}
+
+	void print20()
+	{
+		auto curr = head->next;
+		for (int i = 0; i < 20 && curr != tail; ++i) {
+			std::cout << curr->value << ", ";
+			curr = curr->next;
+		}
+		std::cout << std::endl;
+	}
+
+	bool validate(int x, NODE* p, NODE* c)
+	{
+		auto prev = head;
+		auto curr = prev->next;
+		while (curr->value < x) {
+			prev = curr;
+			curr = curr->next;
+		}
+		return ((prev == p) && (curr == c));
+	}
+
+private:
+	NODE* head, * tail;
+};
+
+O_SET set;
 
 void benchmark(const int num_threads)
 {
